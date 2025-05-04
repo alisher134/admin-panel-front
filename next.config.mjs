a5942bd2b97c6b9d8b2413034c65c1e0fe04407f
alisher134 <alisherrakhmanov134@gmail.com>
2025-05-04 18:42:52 +0500

    feat: add rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'cloudflare-ipfs.com',
-				port: '',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'avatars.githubusercontent.com',
-				port: '',
-				pathname: '/**',
-			},
-		],
-	},
-}
+  async rewrites() {
+    return [
+      {
+        source: "/uploads/:path*",
+        destination: "https://api.datasec.org.kz/uploads/:path*",
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cloudflare-ipfs.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
-export default nextConfig
+export default nextConfig;
