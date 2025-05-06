75ad446498783568619b3225bbfcc71aaa8e0ac8
alisher134 <alisherrakhmanov134@gmail.com>
2025-05-06 23:50:11 +0500

    feat: edit constants

--- src/services/auth/auth.helper.ts
+++ src/services/auth/auth.helper.ts
@@ -11,7 +11,7 @@ export const getAccessToken = () => {
 };
 
 export const saveTokenStorage = (accessToken: string) => {
-  const domain = process.env.NEXT_PUBLIC_DOMAIN;
+  const domain = "datasec.org.kz";
 
   Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, {
     domain,
